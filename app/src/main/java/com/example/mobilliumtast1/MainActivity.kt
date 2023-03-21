2405eb5621ccd1d1eb503f276683ac0d5e556dd7
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-03-21 15:43:49 +0300

    dark mode added

--- app/src/main/java/com/example/mobilliumtast1/MainActivity.kt
+++ app/src/main/java/com/example/mobilliumtast1/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.mobilliumtast1
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //sharedPreferences kullanmamızın amacı uygulamayı açıp tekrar açtığımzda mode durumunu kaydetmek
+        val sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
+        var mode = sharedPreferences.getBoolean("night", false) //light mode is the default mode
+        val editor = sharedPreferences.edit()
+
+        if (mode) {
+            switcher.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switcher.setOnClickListener {
+            if (mode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+            }
+            editor.apply()
+        }
+
     }
 }
